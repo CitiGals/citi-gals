51b6e3efee7f98490b7dbe39e16a24f98257e30a
emahannon <georgiannahannon@gmail.com>
2019-06-07 16:41:45 -0400

    added flexbox to header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import {Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
+import { TeamComponent } from './about/team.component';
 import { ResourcesComponent } from './resources/resources.component';
 
 
 const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'resources', component: ResourcesComponent},
-    {path: 'about', component: AboutComponent}
+    {path: 'team', component: TeamComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
+import { TeamComponent } from './about/team.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
@@ -17,7 +17,7 @@ import { CarouselComponent } from './carousel/carousel.component';
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
+    TeamComponent,
     FooterComponent,
     HeaderComponent,
     HomeComponent,
