f1b64d0b537c6a30104acbcb3c0fcbc0d51b87d7
emahannon <georgiannahannon@gmail.com>
2019-06-07 14:47:44 -0400

    fixed routing (again)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import {Routes, RouterModule } from '@angular/router';
 
-import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 import { ResourcesComponent } from './resources/resources.component';
 
 
 const routes: Routes = [
-    {path: 'home', component: HomeComponent},
+    {path: '', component: HomeComponent},
     {path: 'resources', component: ResourcesComponent},
     {path: 'about', component: AboutComponent}
 ];
