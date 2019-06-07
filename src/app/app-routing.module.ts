a00b1d299e3b274ebe95768338d16db336ca4b13
emahannon <georgiannahannon@gmail.com>
2019-06-07 09:31:21 -0400

    header links added via angular routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import {Routes, RouterModule } from '@angular/router';
 
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ResourcesComponent } from './resources/resources.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'resources', component: ResourcesComponent},
+    {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
