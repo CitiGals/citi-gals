f27fc1713f3eaf0a4674f7cc273b2d3ef45c3258
emahannon <georgiannahannon@gmail.com>
2019-06-07 13:50:27 -0400

    basic carousel component with ng-bootstrap implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { ResourcesComponent } from './resources/resources.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 
 @NgModule({
@@ -18,12 +21,14 @@ import { ResourcesComponent } from './resources/resources.component';
     FooterComponent,
     HeaderComponent,
     HomeComponent,
-    ResourcesComponent
+    ResourcesComponent,
+    CarouselComponent
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
