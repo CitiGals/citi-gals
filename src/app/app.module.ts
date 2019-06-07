774fc442f7a781bd215c02ec89968802b900fae7
emahannon <georgiannahannon@gmail.com>
2019-06-07 09:17:27 -0400

    create other components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ResourcesComponent } from './resources/resources.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    AboutComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    ResourcesComponent
+
   ],
   imports: [
     BrowserModule,
