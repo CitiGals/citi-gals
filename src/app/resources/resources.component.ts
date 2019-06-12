8b8579267964477dccead37203c1a60abd33fefb
emahannon <georgiannahannon@gmail.com>
2019-06-12 16:21:33 -0400

    idk y this is broken but it is

--- src/app/resources/resources.component.ts
+++ src/app/resources/resources.component.ts
@@ -8,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class ResourcesComponent implements OnInit {
   constructor() { }
 
-  links = [{title: 'Stephanie Hannon', text: 'Team Lead', photo: '/assets/images/LogoCircle.png', link: 'https://www.girlpowered.com/'},
+  links = [{title: 'Stephanie Hannon', text: 'Team Lead',
+            photo: '/assets/images/Web page samples and media/Resources link icons/All Finished icons/GPLOGO SNIP beveledTRANSPARENT.png',
+            link: 'https://www.girlpowered.com/'},
   {title: 'Stephanie Hannon', text: 'Team Lead', photo: '/assets/images/LogoCircle.png', link: 'https://www.girlpowered.com/'},
   {title: 'Stephanie Hannon', text: 'Team Lead', photo: '/assets/images/LogoCircle.png', link: 'https://www.girlpowered.com/'},
   {title: 'Stephanie Hannon', text: 'Team Lead', photo: '/assets/images/LogoCircle.png', link: 'https://www.girlpowered.com/'},
