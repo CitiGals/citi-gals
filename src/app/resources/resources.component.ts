2084424afa62c7cb5d756b60bcd4485a457738d0
emahannon <georgiannahannon@gmail.com>
2019-06-13 16:12:37 -0400

    resources page fixes

--- src/app/resources/resources.component.ts
+++ src/app/resources/resources.component.ts
@@ -8,13 +8,28 @@ import { Component, OnInit } from '@angular/core';
 export class ResourcesComponent implements OnInit {
   constructor() { }
 
-  links = [{title: 'Stephanie Hannon', text: 'Team Lead',
+  links = [{title: 'Take the Girl Powered Pledge', 
+            text: 'Pledge to increase the number of women involved in STEM and robotics!',
             photo: '/assets/images/Web page samples and media/Resources link icons/All Finished icons/GPLOGO SNIP beveledTRANSPARENT.png',
             link: 'https://www.girlpowered.com/'},
-  {title: 'Stephanie Hannon', text: 'Team Lead', photo: '/assets/images/LogoCircle.png', link: 'https://www.girlpowered.com/'},
-  {title: 'Stephanie Hannon', text: 'Team Lead', photo: '/assets/images/LogoCircle.png', link: 'https://www.girlpowered.com/'},
-  {title: 'Stephanie Hannon', text: 'Team Lead', photo: '/assets/images/LogoCircle.png', link: 'https://www.girlpowered.com/'},
-  {title: 'Stephanie Hannon', text: 'Team Lead', photo: '/assets/images/LogoCircle.png', link: 'https://www.girlpowered.com/'}
+
+  {title: 'REC Foundation',
+  text: 'Explore the website! There are links and tutorials to almost any question you can think of.',
+  photo: '/assets/images/Web page samples and media/Resources link icons/All Finished icons/REC Foundation beveled.png',
+  link: 'https://www.roboticseducation.org/competition-teams/vex-robotics-competition/'},
+
+  {title: 'RobotEvents Map',
+  text: 'Find robotics competitions in your area!',
+  photo: '/assets/images/Web page samples and media/Resources link icons/All Finished icons/vex map.png',
+  link: 'https://www.girlpowered.com/'},
+
+  {title: 'Stephanie Hannon', text: 'Team Lead',
+  photo: '/assets/images/Web page samples and media/Resources link icons/All Finished icons/vex robotics competition guide.png',
+  link: 'https://www.girlpowered.com/'},
+
+  {title: 'Stephanie Hannon', text: 'Team Lead',
+  photo: '/assets/images/Web page samples and media/Resources link icons/All Finished icons/vex startup guidetransparent.png',
+  link: 'https://www.girlpowered.com/'}
 ];
 
   ngOnInit() {
